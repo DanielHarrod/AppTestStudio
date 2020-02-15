a3904c9d9c6e7b6ad4730c6d14ff1f57ea50d64a
Daniel Harrod <djharrod@gmail.com>
2020-02-15 11:53:00 -0600

    New Form

---
+++ AppTestStudio/frmMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AppTestStudio
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
