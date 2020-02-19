cbf6fd63062c0740c0fb93bfe96f6aa5ea2506bb
Daniel Harrod <djharrod@gmail.com>
2020-02-18 20:35:17 -0600

    Schedule

--- AppTestStudio/Schedule.cs
+++ AppTestStudio/Schedule.cs
@@ -1,17 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace AppTestStudio
 {
     public class Schedule
     {
-        public String ID { get; set; }
         public Boolean IsEnabled { get; set; }
 
         public List<ScheduleItem> ScheduleList { get; set; }
+
+        public String GetFileName()
+        {
+            return Utils.GetApplicationFolder() + @"\Schedule.xml";
+        }
+
+
+        public void Load()
+        {
+            String FileName = GetFileName();
+
+            if (System.IO.File.Exists(FileName))
+            {
+                Schedule XMLSchedule = new Schedule();
+                XmlSerializer Serializer = new XmlSerializer(GetType());
+                TextReader TRead = new StreamReader(FileName);
+                XMLSchedule = Serializer.Deserialize(TRead) as Schedule;
+
+                IsEnabled = XMLSchedule.IsEnabled;
+                ScheduleList = XMLSchedule.ScheduleList;
+
+                TRead.Close();
+            }
+            else
+            {
+                IsEnabled = false;
+
+            }
+        }
+
+        public void Save()
+        {
+            String FileName = GetFileName();
+
+            StreamWriter SR = new StreamWriter(FileName);
+            XmlSerializer Serializer = new XmlSerializer(GetType());
+
+            Serializer.Serialize(SR, this);
+            SR.Close();
+        }
 
     }
 }
