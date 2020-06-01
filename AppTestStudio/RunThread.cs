24fcb1375a6ff74b8d196896fcb81fc611b94181
Daniel Harrod <djharrod@gmail.com>
2020-05-31 23:02:30 -0500

    Updated recycle to include same window and app.

--- AppTestStudio/RunThread.cs
+++ AppTestStudio/RunThread.cs
@@ -535,7 +535,7 @@ namespace AppTestStudio
         {
             StopThreadCloseWindow(WindowHandle, false);
             GameNodeGame Game = node.GetGameNodeGame();
-            Utils.LaunchInstance("", "", Game.InstanceToLaunch, Game.Resolution,Game.DPI);
+            Utils.LaunchInstance(Game.PackageName, Game.TargetWindow, Game.InstanceToLaunch, Game.Resolution,Game.DPI);
         }
 
         private AfterCompletionType CheckLimit(GameNodeAction node)
