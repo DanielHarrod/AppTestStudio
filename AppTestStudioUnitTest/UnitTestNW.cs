d4ad1c6d0b771fff46b00fc2dc5b0f364a6fecaa
Daniel Harrod <djharrod@gmail.com>
2022-04-28 09:39:58 -0500

    Some NW testing.

--- AppTestStudioUnitTest/UnitTestNW.cs
+++ AppTestStudioUnitTest/UnitTestNW.cs
@@ -11,13 +11,18 @@ namespace AppTestStudioUnitTest
         public void TestMethod1()
         {
             System.Threading.Thread.Sleep(1000);
-            IntPtr k = AppTestStudio.Utils.GetWindowHandleByWindowName("World of New :)", "");
+            IntPtr k = AppTestStudio.Utils.GetWindowHandleByWindowName("World of New", "");
 
             API.GetCursorPos(out API.Point point);
             short xStart = (short) point.X;
             short yStart = (short) point.Y;
-            Utils.ClickOnWindowActiveMode(k, xStart, yStart, 100);
+            //Utils.ClickOnWindow(k,MouseMode.Active,false)
+            //Utils.ClickOnWindowActiveMode(k, xStart, yStart, 100);
+            Utils.ClickOnWindowActiveMode(k, 1920, 1096, 200);
+            System.Threading.Thread.Sleep(4000);
+            Utils.ClickOnWindowActiveMode(k, 1920, 1096, 100);
 
+            return;
             System.Threading.Thread.Sleep(2000);
 
             //System.Drawing.Bitmap b = AppTestStudio.Utils.GetBitmapFromWindowHandle(k);
