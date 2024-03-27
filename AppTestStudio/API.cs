ce66e5c5c209b4e7d867799778682a9df3ab76ea
Daniel Harrod <djharrod@gmail.com>
2024-03-26 22:17:21 -0500

    Added GetDesktopWindow import for testing.
    Testing Selecting entire Desktop space instead of specifying a window.

--- AppTestStudio/API.cs
+++ AppTestStudio/API.cs
@@ -3,11 +3,8 @@
 //This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or(at your option) any later version.  This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program. If not, see<https://www.gnu.org/licenses/>.
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace AppTestStudio
 {
@@ -220,5 +217,8 @@ namespace AppTestStudio
         [DllImport("user32.dll")]
         public static extern IntPtr WindowFromPoint(System.Drawing.Point p);
 
+
+        [DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
     }
 }
