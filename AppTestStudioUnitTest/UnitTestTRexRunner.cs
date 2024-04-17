c15eb0ed04d9b4f292d39050ff2cf7e94a6f6849
Daniel Harrod <djharrod@gmail.com>
2024-04-16 22:56:45 -0500

    Testing - targeted test Trex-runner

---
+++ AppTestStudioUnitTest/UnitTestTRexRunner.cs
@@ -0,0 +1,44 @@
+﻿using AppTestStudio;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace AppTestStudioUnitTest
+{
+    [TestClass]
+    public class UnitTestTRexRunner
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AppTestStudio.KeyboardProcessor kp = new AppTestStudio.KeyboardProcessor();
+            String s = " ";
+
+            KeyboardCommand[] list = kp.ParseScript(s).ToArray();
+
+            GameNodeAction ActionNode = new GameNodeAction("", ActionType.Action);
+            ActionNode.KeyboardBetweenDuration = 30;
+            ActionNode.KeyboardBetweenDurationRandom = 3;
+
+            ActionNode.KeyboardDownDuration = 50;
+            ActionNode.KeyboardDownDurationRandom = 5;
+
+
+            List<KeyboardCommand> CompleteList = kp.SequenceAndApplyPreWaits(list, ActionNode);
+            for (int i = 0; i < 10; i++)
+            {
+                foreach (KeyboardCommand command in CompleteList)
+                {
+                    Utils.ProcessKeyboardCommand(command);
+                    Debug.WriteLine(command);
+                }
+                System.Threading.Thread.Sleep(1000);
+
+            }
+
+
+
+        }
+    }
+}
